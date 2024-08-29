8b1c66c3e184495a8b2b3098870bc38cdc17ac6e
Max <max@webmaestro.com.ua>
2024-08-29 14:26:12 +0300

    adds proofs

--- client/index.js
+++ client/index.js
@@ -4,14 +4,17 @@ const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
 
-async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+const ListMerkleTree = new MerkleTree(niceList);
+
+async function main(name) {
+  const proof = ListMerkleTree.getProof(niceList.indexOf(name));
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    proof: proof,
+    name: name,
   });
 
   console.log({ gift });
 }
 
-main();
+main("Anna Stehr");
